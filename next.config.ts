28d4cb270937b3ea03ea93f170a34f94f39106f9
linhmon <nguyenlinhmon@gmail.com>
2025-03-08 22:25:50 +0900

    fix image skinking

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix:'https://linhmonkaist/semscholars',
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
