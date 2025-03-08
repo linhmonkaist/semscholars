0589c5ec0f4618e8d61c29e6338d5238fc8f50f1
linhmon <nguyenlinhmon@gmail.com>
2025-03-08 21:42:42 +0900

    add assetprefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix:'https://linhmonkaist/semscholars',
+  output: 'export',
   images: {
     unoptimized: true,
   },
