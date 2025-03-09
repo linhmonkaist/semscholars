e1cf6b36d0e984a53b59ab2771979e26c13740b4
linhmon <nguyenlinhmon@gmail.com>
2025-03-09 21:45:38 +0900

    fix build error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AppSidebar } from "../../components/app-sidebar"
 import {
   Breadcrumb,
@@ -9,11 +11,12 @@ import {
 } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { data } from "../courses/sidebar-data"
 
 export default function Page() {
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar data={data}/>
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
