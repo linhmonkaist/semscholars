c4b21c34d0be77ddd6eb2827640af57916a70b06
linhmon <nguyenlinhmon@gmail.com>
2025-03-12 12:56:51 +0900

    fix config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
 const nextConfig: NextConfig = {
   basePath: isGithubPages ? '/semscholars' : '',
   assetPrefix: isGithubPages ? '/semscholars/' : '',
+  output: 'export',
 };
 
 export default nextConfig;
