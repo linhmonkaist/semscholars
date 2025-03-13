563a5965f2113e19633a6ac557cb59d9fc33f1ce
linhmon <nguyenlinhmon@gmail.com>
2025-03-14 00:17:19 +0900

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const isGithubPages = process.env.DEPLOY_TARGET === 'github-pages';
 
 const nextConfig: NextConfig = {
-  basePath: isGithubPages ? 'https://linhmonkaist.github.io/semscholars' : '',
-  // assetPrefix: isGithubPages ? '/semscholars/' : '',
+  basePath: isGithubPages ? '/semscholars' : '',
   output: 'export',
 };
 
